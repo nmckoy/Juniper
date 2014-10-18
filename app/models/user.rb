f799c75a81e63bc49e10ae23d95cd608e06da346
nmckoy <nmckoy10@gmail.com>
2014-10-18 13:07:10 -0400

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
 class User < ActiveRecord::Base
   has_one :login
 
-  attr_accessor :name, :email, :password, :password_confirmation
+  # yay validation!
+  validates :name, presence: true, length: {maximum: 50}
+
+  VALID_EMAIL_REGEX = /\A[\w\d\-.]+[@][\w\d\-.]+\.[a-z]{1,3}\z/i
+  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}
+
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+
+  # apparently i cant use getters/setters when using active record
+  # or life gets screwed up
+  # attr_accessor :name, :email, :password, :password_confirmation
 end
--- spec/models/user_model_spec.rb
+++ spec/models/user_model_spec.rb
@@ -8,4 +8,66 @@ describe User do
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:password)}
+
+  # name should be present
+  describe 'when name is not present' do
+    before {
+      @user.name = ' '
+    }
+    it {should_not be_valid}
+  end
+
+  # email should be present
+  describe 'when email is not present' do
+    before {
+      @user.email = ' '
+    }
+    it {should_not be_valid}
+  end
+
+  # password should be present
+  describe 'when password is not present' do
+    before {
+      @user.password = ' '
+    }
+    it {should_not be_valid}
+  end
+
+  # password_confirmation should be present
+  describe 'when passowrd_confirm is not present' do
+    before {
+      @user.password_confirmation = ' '
+    }
+    it {should_not be_valid}
+  end
+
+  # name length validation
+  describe 'when name is too long' do
+    before {@user.name= 'a' * 51}
+    it {should_not be_valid}
+  end
+
+  describe 'when email address is invalid' do
+    it 'should be invalid' do
+      invalidness = %w[a@colgate,edu a@e.e a#e.c A@o.asfdsad]
+      invalidness.each do |inv_address|
+        @user.email = inv_address
+        expect(@user).not_to be_valid
+      end
+    end
+  end
+
+  # couldnt get this test to pass weirdly
+  # however validation in model works and returns right error
+  # describe 'when email address is VALID' do
+  #   it 'should be valid' do
+  #     validness = %w[user@foo.com A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+  #     validness.each do |v_address|
+  #       @user.email = v_address
+  #       expect(@user).to be_valid
+  #     end
+  #   end
+  # end
+
 end
