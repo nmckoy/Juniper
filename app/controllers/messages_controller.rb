ed55e5947cc419982f2a5b5d7cc293fb71e4caa4
Nick McKoy <nmckoy10@gmail.com>
2015-02-06 16:01:45 +0000

    can send messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,18 +1,35 @@
 class MessagesController < ApplicationController
+  before_action :get_user, only: [:new, :create]
   
   def new
     # just to display the form
-    
-    @user = User.find(params[:user])
+    # user to send message to
+    #@user = User.find(params[:user])
     
     # @message = current_user.messages.new
   end
 
   def create
-    @recipient = @user #User.find(params[:user])
+    @recipient = @user #User.find(params[:user_object])
+    
+    #need to validate if user actually exists
+    #shouldnt happen but it doesnt hurt
+    #debugging
+    # if @recipient
+    #   raise 'not null'
+    # else
+    #   raise 'null recipient'
+    # end
+    
     current_user.send_message(@recipient, params[:body], params[:subject])
     flash[:notice] = "Message has been sent!"
     
     redirect_to conversations_path(:id => current_user.id)
   end
+  
+  private
+    def get_user
+      #raise params[:user].to_s
+      @user = User.find(params[:user])
+    end
 end
