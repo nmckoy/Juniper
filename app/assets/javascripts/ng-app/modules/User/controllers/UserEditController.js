0569060fdd7cf17f833becf5865ed1517ec75ab6
Nick McKoy <nmckoy10@gmail.com>
2015-03-22 17:03:13 +0000

    moved user angular files into vertical module

---
+++ app/assets/javascripts/ng-app/modules/User/controllers/UserEditController.js
@@ -0,0 +1,71 @@
+(function() {
+  
+  var UserEditController = function($scope, User, $log, $routeParams,
+                                    $location) {
+    $scope.error = null;
+    //$log.info("in usereditctrl");
+    
+    $scope.edit_name_flag = null;
+    $scope.edit_email_flag = null;
+    // opening input text for editing
+    $scope.edit_name = function() {
+      $scope.edit_name_flag = true;
+    };
+    $scope.edit_email = function(){
+      $scope.edit_email_flag = true;
+    };
+    
+    // watching my flags change
+    $scope.$watch("edit_name_flag", function() {
+      if ($scope.edit_name_flag === false) {
+        update("name");
+      }
+    });
+    $scope.$watch("edit_email_flag", function() {
+      if ($scope.edit_email_flag === false){
+        update("email");
+      }
+    });
+    
+    var update = function(prop){
+      User.get({id: $routeParams.id}, function(user_edit){
+        var saved_name = user_edit.name;
+        var saved_email = user_edit.email;
+        if (prop === "name") {
+          user_edit.name = $scope.user.name;
+        } else if (prop === "email") {
+          user_edit.email = $scope.user.email;
+        }
+        
+        // $log.info("new user name is " + user_edit.name);
+        // $log.info("name " + user_edit.name + 
+        //          "\nemail " + user_edit.email);
+        
+        // resource instace methods return $promises
+        user_edit.$update().then(function(data) {
+          // success
+          // var datatype = typeof data
+          $log.info("updated successfully with data: "
+                      +  data.data);
+          // this is why callbacks are important
+          //$location.path("/profile/" + $scope.user.id);
+        }, function(error) {
+          // error
+          // var errortype = typeof error;
+          $log.error("error updating user with error: "
+                      + error.data);
+          $scope.user.name = saved_name;
+          $scope.user.email = saved_email;
+          $scope.error = error.data;
+          //$location.path("/profile/" + $scope.user.id + "/edit");
+        }); // end update
+      }); // end get
+    };
+    
+  };
+  
+  angular.module("_USR")
+    .controller("UserEditController", UserEditController);
+    
+  
+}());
---
+++ app/assets/javascripts/ng-app/modules/User/controllers/UserShowController.js
@@ -0,0 +1,37 @@
+(function() {
+  
+  var UserShowController = function($scope, User, Slog, $log, 
+                                  $routeParams, $location) {
+                                      
+    //$log.info("in profilectrl");
+    
+    $scope.user_slogs = [];
+    User.get({id: $routeParams.id})
+      .$promise.then(function(data) {
+        // success
+        $scope.user = data;
+      }, function(error) {
+        $log.info(error.status);
+        if (error.status === 404) {
+          $location.path("/404");
+        }
+    });
+    
+    Slog.get({user_slogs: true, user_id: $routeParams.id}, 
+      function(){
+      //$log.info("get callback");
+      // $log.info($scope.slogs);
+    }).$promise.then(function(data){
+      //$log.info("promise callback");
+      //$log.info("slogs" + data.slogs);
+      $scope.user_slogs = data.slogs;
+    }, function(error) {
+      // error handling
+      $scope.user_slogs = null;
+    });
+  };
+  
+  angular.module("_USR")
+    .controller("UserShowController", UserShowController);
+  
+}());
